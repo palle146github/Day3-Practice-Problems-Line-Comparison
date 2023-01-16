7f6c97d4ec0ffdf969b0db6dd953e47e5006408f
palle146github <prasanthpalle146@gmail.com>
2023-01-16 23:16:30 +0530

    Refactor: Using Java OOPs concept to CompareTo method

--- src/Main.java
+++ src/Main.java
@@ -1,49 +1,36 @@
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Welcome to Line comparison problems");
+        System.out.println("ax^2 + bx + c");
+        double a, b, c;
+        System.out.println("Enter constants of the quadratic expression");
         Scanner s = new Scanner(System.in);
-        double ax1,ay1,ax2,ay2,x4,bx1,by1,bx2,by2;
-        String lengthoftheline1, lengthoftheline2;
-        System.out.println("Enter x1 Co-ordinate of first line");
-        ax1 = s.nextDouble();
-        System.out.println("Enter y1 Co-ordinate of first line");
-        ay1 = s.nextDouble();
-        System.out.println("Enter x2 Co-ordinate of first line");
-        ax2 = s.nextDouble();
-        System.out.println("Enter y2 Co-ordinate of first line");
-        ay2 = s.nextDouble();
-        lengthoftheline1 = String.valueOf(Math.sqrt( Math.pow(( ax2 - ax1), 2) + Math.pow(( ay2 - ay1 ), 2 )));
-
-        System.out.println("Enter x1 Co-ordinate of second line");
-        bx1 = s.nextDouble();
-        System.out.println("Enter y1 Co-ordinate of second line");
-        by1 = s.nextDouble();
-        System.out.println("Enter x2 Co-ordinate of second line");
-        bx2 = s.nextDouble();
-        System.out.println("Enter y2 Co-ordinate of second line");
-        by2 = s.nextDouble();
-        lengthoftheline2 = String.valueOf(Math.sqrt( Math.pow(( bx2 - bx1), 2) + Math.pow(( by2 - by1 ), 2 )));
-
-        System.out.println("Length of the first line is :" + lengthoftheline1);
-        System.out.println();
-        System.out.println("Length of the second line is :" + lengthoftheline2);
-        System.out.println();
-        System.out.println("Comparing the equality of the lines of based on end points");
-        String diffinline1 =String.valueOf(Math.floor((bx2-ax2)));
-        String diffinline2 =String.valueOf(Math.floor((by2-ay2)));
-        double difference;
-        if(lengthoftheline1.compareTo(lengthoftheline2) == 1) {
-            difference= (Double.valueOf(lengthoftheline1))- (Double.valueOf(lengthoftheline2));
-            System.out.println("Line 1 is longer than line 2 by " +difference);
-        } else if(lengthoftheline1.compareTo(lengthoftheline2) == 0) {
-            System.out.println("Both lines are equal");
-        } else  {
-            difference= (Double.valueOf(lengthoftheline2))- (Double.valueOf(lengthoftheline1));
-            System.out.println("Line 2 is longer than line 1 by " +difference);
+        System.out.println("Enter a value ==> ");
+        a = s.nextInt();
+        System.out.println("Enter b value ==> ");
+        b = s.nextInt();
+        System.out.println("Enter c value ==> ");
+        c = s.nextInt();
+        double root1, root2;
+        double v = -4 * a * c;
+        double delta = Math.pow( b, 2 ) - ( 4*a*c );
+        double determinant = Math.sqrt((Math.pow( b, 2 )) - 4*a*c);
+        if ( determinant >= 0) {
+            root1 = ( -b + determinant ) / (2 * a);
+            root2 = ( -b - determinant) / (2 * a);
+            System.out.println("Roots of the x are ==>");
+            System.out.println("Root one is " + root1);
+            System.out.println("Root two is " + root2);
         }
-
+        else{
+            double rt = -5;
+            System.out.println(Math.abs(rt));
+            double ex1, ex2, ex3;
+            ex1 = ( - b ) / (2 * a);
+            ex2 = Math.abs(delta);
+            ex3 = Math.sqrt(ex2) / (2 * a);
+            System.out.println("Root one is " + ex1 + " + " + "i" + ex2);
+            System.out.println("Root two is " + ex1 + " - " + "i" + ex2);
+        }
     }
 }
--- src/com/bridgelabz/linecomparisonproblems/CompareToUsingoops.java
+++ src/com/bridgelabz/linecomparisonproblems/CompareToUsingoops.java
@@ -1,2 +1,43 @@
-package com.bridgelabz.linecomparisonproblems;public class CompareTo {
+package com.bridgelabz.linecomparisonproblems;
+
+public class CompareToUsingoops {
+    private double x1, y1, x2, y2;
+
+    public CompareToUsingoops() {
+
+    }
+
+    public CompareToUsingoops(double x1, double y1, double x2, double y2) {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+    }
+
+    public double lengthOftheLine() {
+        double lengthoftheline = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+        return lengthoftheline;
+    }
+
+    static void compareTo(Double lengthoftheline1, Double lengthoftheline2) {
+        if (lengthoftheline1.compareTo(lengthoftheline2) == 1) {
+            double difference = (Double.valueOf(lengthoftheline1)) - (Double.valueOf(lengthoftheline2));
+            System.out.println("Line 1 is longer than line 2 by " + difference);
+        } else if (lengthoftheline1.compareTo(lengthoftheline2) == 0) {
+            System.out.println("Both lines are equal");
+        } else {
+            double difference = (Double.valueOf(lengthoftheline2)) - (Double.valueOf(lengthoftheline1));
+            System.out.println("Line 2 is longer than line 1 by " + difference);
+        }
+    }
+
+    public static void main(String[] args) {
+        CompareToUsingoops length1 = new CompareToUsingoops(5, 7, 3, 9);
+        Double lengthOfLine1 = length1.lengthOftheLine();
+        CompareToUsingoops length2 = new CompareToUsingoops(6, 3, 3, 8);
+        Double lengthOfLine2 = length2.lengthOftheLine();
+        compareTo(lengthOfLine1, lengthOfLine2);
+    }
 }
+
+
